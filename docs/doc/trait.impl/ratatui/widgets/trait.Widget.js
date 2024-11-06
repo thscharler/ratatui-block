5a179670331b483e7a8e75805f164348beec1e43
thscharler <thscharler@gmail.com>
2024-11-06 06:19:36 +0100

    feat: draw up v3

---
+++ docs/doc/trait.impl/ratatui/widgets/trait.Widget.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["ratatui_block",[["impl Widget for &amp;<a class=\"struct\" href=\"ratatui_block/struct.Joint.html\" title=\"struct ratatui_block::Joint\">Joint</a>"],["impl Widget for <a class=\"struct\" href=\"ratatui_block/struct.Joint.html\" title=\"struct ratatui_block::Joint\">Joint</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[282]}
