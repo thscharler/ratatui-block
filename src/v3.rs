f933be93d4a9e69513049a29c2f9699ad93a988d
thscharler <thscharler@gmail.com>
2024-11-06 07:59:54 +0100

    feat: extend on v3 even more

--- src/v3.rs
+++ src/v3.rs
@@ -161,21 +161,41 @@ pub enum Side {
 
 /// Positions along one side of the block's area.
 ///
+/// Examples below area for Plain/Plain borders.
+/// This leads to duplicate glyphs here, but for other
+/// BorderTypes such as QuadrantInside those would
+/// be different glyphs. ... more later ...
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Kind {
     /// Regular start corner.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┌`
     RegularStart,
     /// Draw a 90° outward joint at the start.
     ///
     /// Connects the start corner of this area with the
     /// start corner of the other area.
+    ///
+    /// __Example for Top __
+    ///
+    /// `├`
     AngleStartStart,
     /// Draw a 90° outward joint at the start.
     ///
     /// Connects the start corner of this area with the
     /// end corner of the other area.
+    ///
+    /// __Example for Top__
+    ///
+    /// `├`
     AngleStartEnd,
     /// Prolong the border along the side before start.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┬`
     ProlongStart,
     /// Draw a cross joint at the start of the side.
     ///
@@ -184,11 +204,19 @@ pub enum Kind {
     ///
     /// The border at 90° outward from the side is
     /// [BorderGlyph::other_border](BorderGlyph::other_border).
+    ///
+    /// __Example for Top__
+    ///
+    /// `┼`
     CrossStart(BorderType),
 
     /// Regular border at position.
     ///
     /// Position 0 and width-1 are auto-converted to Start/End.
+    ///
+    /// __Example for Top__
+    ///
+    /// `─`
     Regular,
     /// Draw a 90° outward joint at some position.
     ///
@@ -196,6 +224,10 @@ pub enum Kind {
     /// right side of the other area. This joins the start.
     ///
     /// Position 0 and width-1 are auto-converted to Start/End.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┴`
     AngleOutwardStart,
     /// Draw a 90° outward joint at some position.
     ///
@@ -203,37 +235,72 @@ pub enum Kind {
     /// right side of the other area. This joins the end.
     ///
     /// Position 0 and width-1 are auto-converted to Start/End.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┴`
     AngleOutwardEnd,
     /// Draw a 90° inward joint at some position.
     ///
     /// Position 0 and width-1 are auto-converted to Start/End.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┬`
     AngleInwardStart,
     /// Draw a 90° inward joint at some position.
     ///
     /// Position 0 and width-1 are auto-converted to Start/End.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┬`
     AngleInwardEnd,
     /// Draw a cross joint at some position.
     ///
     /// The border type here is the border at 90° to the inward.
     /// The border at 90° outward from the side is Joint::other_side.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┼`
     Cross(BorderType),
     /// ?? maybe ??
     /// Overlap of the two borders.
+    ///
+    /// __Example for Top__
+    ///
+    /// `─`
     Overlap(BorderType),
-
     /// Regular end corner.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┐`
     RegularEnd,
     /// Draw a 90° outward joint at the end.
     ///
     /// Connects the end corner of this area with the
     /// start corner of the other area.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┤`
     AngleEndStart,
     /// Draw a 90° outward joint at the end.
     ///
     /// Connects the end corner of this area with the
     /// end corner of the other area.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┤`
     AngleEndEnd,
     /// Prolong the border along the side after the end.
+    ///
+    /// __Example for Top__
+    ///
+    /// `┬`
     ProlongEnd,
     /// Draw a cross joint at the end.
     ///
@@ -242,5 +309,8 @@ pub enum Kind {
     ///
     /// The border at 90° outward from the side is
     /// [BorderGlyph::other_border](BorderGlyph::other_border).
+    /// __Example for Top__
+    ///
+    /// `┼`
     CrossEnd(BorderType),
 }
